668a321b64ddd010666c0a167938fdccc6500d20
Seniormarfo <polmbre@gmail.com>
2025-04-21 21:27:38 +0000

    Update kofi.cpp

--- kofi.cpp
+++ kofi.cpp
@@ -1,21 +1,57 @@
-#include <ionstream>
+
+#include <iostream>
 using namespace std;
 
 int main(){
-    int number 1;
-    int number 2;
+    int number1;
+    int number2;
     int result;
 
-    char operation;
+    int operation;
 
     cout <<"Enter first number (between 0 to 9):";
-    cin >> number 1;
+    cin >> number1;
 
-    cout <<"Enter Operation (+,-,*,/):";
+    cout <<"Enter Operation:\n";
+    cout <<"Enter 1 for Addition:\n";
+    cout <<"Enter 2 for Subtraction:\n";
+    cout <<"Enter 3 for multiplication:\n";
+    cout <<"Enter 4 for Division:\n";
+   
     cin >> operation;
 
     cout <<"Enter Second number (between 0 to 9):";
-    cin >> number 2;
+    cin >> number2;
+     
 
-    cout <<number 1<<" "<<operation<<" "<<number 2<< endl
+switch (operation)
+{
+case 1:
+cout <<number1<<" "<<"+"<<" "<<number2<< endl;
+result = number1 + number2;
+    break;
+
+case 2:
+cout <<number1<<" "<<"-"<<" "<<number2<< endl;
+result = number1 - number2;
+    break;
+    
+case 3:
+cout <<number1<<" "<<"*"<<" "<<number2<< endl;
+result = number1 * number2;
+    break;
+
+    case 4:
+cout <<number1<<" "<<"/"<<" "<<number2<< endl;
+result = number1 / number2;
+    break;
+
+default:
+cout << "Invalid Response"<< endl;
+    break;
+}
+        
+    cout <<"result =  "<<result<<endl;
+
+cout<< "Thank You !!!"<< endl;
 }
