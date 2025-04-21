c423e03b046a15044afc0be656b4563448817159
Seniormarfo <polmbre@gmail.com>
2025-04-21 21:02:52 +0000

    Add files via upload

---
+++ kofi.cpp
@@ -0,0 +1,21 @@
+#include <ionstream>
+using namespace std;
+
+int main(){
+    int number 1;
+    int number 2;
+    int result;
+
+    char operation;
+
+    cout <<"Enter first number (between 0 to 9):";
+    cin >> number 1;
+
+    cout <<"Enter Operation (+,-,*,/):";
+    cin >> operation;
+
+    cout <<"Enter Second number (between 0 to 9):";
+    cin >> number 2;
+
+    cout <<number 1<<" "<<operation<<" "<<number 2<< endl
+}
